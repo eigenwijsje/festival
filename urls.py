de86e91b9fc573fa2d146917616572c06cdc4ed2
Ernesto Rico Schmidt <e.rico.schmidt@gmail.com>
2009-03-09 21:06:23 +0100

    updated main URL configuration.

--- urls.py
+++ urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
+from django.views.static import serve
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/(.*)', admin.site.root),
     (r'^accounts/', include('registration.urls')),
     (r'^contact/', include('contact_form.urls')),
     (r'^installfest/', include('installfest.urls')),
+    (r'^sitemedia/(.*)', serve, {'document_root': 'sitemedia'}),
 )
